ec457f48ab61281b393f606cbd2ab7a881eef99e
Mimispons <z.lichen@uandresbello.edu>
2025-10-04 02:55:11 -0300

    Filtro penquita( viendo como mejorarlo)

--- Assets/01_Scripts/MainMenuManager.cs
+++ Assets/01_Scripts/MainMenuManager.cs
@@ -3,14 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Este método carga la escena del juego
-    // Cambia "GameScene" por el nombre exacto de tu escena de juego
+    
     public void PlayGame()
     {
         SceneManager.LoadScene("Main");
     }
 
-    // Este método cierra el juego
+    
     public void QuitGame()
     {
         Application.Quit();
