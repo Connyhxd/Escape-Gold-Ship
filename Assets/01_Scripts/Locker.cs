3e7f57398eb4ffe139867c07aa7a41e970de3adf
Connyhxd <fuhualover666@gmail.com>
2025-10-05 00:55:08 -0300

    puzzles basicos

---
+++ Assets/01_Scripts/Locker.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Locker : MonoBehaviour
+{
+    private bool doorOpened = false;
+
+    public Animator lockerAnim;
+
+    private bool playerNearby = false;
+    public GameObject open;
+
+    public GameObject idkBro;
+
+    private void Update()
+    {
+
+        if (playerNearby && !doorOpened && Input.GetKeyDown(KeyCode.E))
+        {
+            doorOpened = true;
+            lockerAnim.SetTrigger("Opening");
+            open.SetActive(false);
+            idkBro.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Hands"))
+        {
+            playerNearby = true;
+            if (!doorOpened)
+            {
+                open.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Hands"))
+        {
+            playerNearby = false;
+            open.SetActive(false);
+        }
+    }
+
+}
